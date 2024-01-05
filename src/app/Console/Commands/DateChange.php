4c5e8639315ae19e0a582d15e871819a777b1b8d
takeda shigeki <takeda.shigeki0614@gmail.com>
2024-01-05 16:06:49 +0900

    ファイルの追加

---
+++ src/app/Console/Commands/DateChange.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\Time;
+use Carbon\Carbon;
+
+class DateChange extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'command:dateChange';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $today = Carbon::today();
+        $year = intval($today->year);
+        $month = intval($today->month);
+        $day = intval($today->day);
+        Time::where('year',$year)->where('month',$month)->where('day',$day)->where('checkIn',!is_null)->where('checkOut',is_null)->update(['checkOut' => Carbon::now()]);
+    }
+}
--- src/app/Console/Kernel.php
+++ src/app/Console/Kernel.php
@@ -16,6 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->call('App\Http\Controllers\TimeController@breakin')->everyMinute();
     }
 
     /**
@@ -23,10 +24,4 @@ class Kernel extends ConsoleKernel
      *
      * @return void
      */
-    protected function commands()
-    {
-        $this->load(__DIR__.'/Commands');
-
-        require base_path('routes/console.php');
-    }
 }
